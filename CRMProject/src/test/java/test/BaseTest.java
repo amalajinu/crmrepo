baf0a828d5df96d7a3edf90881f7487dd8238ec6
Dell <Dell@DESKTOP-CJLCDUR>
2023-12-11 12:11:08 +0530

    added depenedency

--- CRMProject/src/test/java/test/BaseTest.java
+++ CRMProject/src/test/java/test/BaseTest.java
@@ -3,6 +3,7 @@ package test;
 import org.testng.annotations.Test;
 
 import constants.Constants;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import utility.ElementUtility;
 
 import org.testng.annotations.BeforeMethod;
@@ -31,6 +32,7 @@ public class BaseTest {
   public void beforeMethod(@Optional ("chrome") String browser) {
 	  if(browser.equalsIgnoreCase("chrome"))
 	  {
+		  WebDriverManager.chromedriver().setup();
 		  driver=new ChromeDriver();
 	  }
 	  else if(browser.equalsIgnoreCase("edge"))
